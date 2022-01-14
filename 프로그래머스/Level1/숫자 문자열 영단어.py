53b91b75f066482f69c61a0cb1d907955868d2a1
chan park <60590737+cksl0830@users.noreply.github.com>
2022-01-14 18:38:24 +0900

    Update 숫자 문자열 영단어.py

--- 프로그래머스/Level1/숫자 문자열 영단어.py
+++ 프로그래머스/Level1/숫자 문자열 영단어.py
@@ -16,7 +16,9 @@ def solution(s):
 
     return int(answer)
   
-  //다른사람의 풀이
+    
+    
+//다른사람의 풀이1 (for문 변수2개)
   
   num_dic = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
 
@@ -25,3 +27,15 @@ def solution(s):
     for key, value in num_dic.items():
         answer = answer.replace(key, value)
     return int(answer)
+
+
+//다른 사람의 풀이2 (for문 변수1개)
+
+def solution(s): 
+    dic = {"zero":0,"one":1,"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9} 
+    for i in dic: 
+        s = s.replace(i,str(dic[i])) 
+        
+    return int(s)
+
+
