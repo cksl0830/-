700615cefe7b651cee455b6c1dbf22f81b1b62f7
chan park <60590737+cksl0830@users.noreply.github.com>
2022-01-19 22:45:03 +0900

    Update 체육복.py

--- 프로그래머스/Level1/체육복.py
+++ 프로그래머스/Level1/체육복.py
@@ -1,11 +1,11 @@
 def solution(n, lost, reserve):
 
-    res=set(reserve)-set(lost)
+    res=set(reserve)-set(lost)        # 중복번호 제거 
     los=set(lost)-set(reserve)
 
     for i in res:
         if i-1 in los:
-            los.remove(i-1)
+            los.remove(i-1)          # 왼쪽부터 리스트 제거
         elif i+1 in los:
             los.remove(i+1)
 
