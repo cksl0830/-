62bb91b517ec12f47ea6d172c84da33ed887ef71
chan park <60590737+cksl0830@users.noreply.github.com>
2022-01-24 20:40:44 +0900

    1.24일

---
+++ 프로그래머스/Level1/다트게임.py
@@ -0,0 +1,56 @@
+def solution(dartResult):
+    temp = ''
+    score = []
+    
+    for i in dartResult:
+        if i.isnumeric():
+            temp += i   #처음에 temp=i로 했는데 10 이라는 숫자때문에 에러남  
+        elif i == 'S':
+            temp = int(temp)**1
+            score.append(temp)
+            temp = ''
+        elif i == 'D':
+            temp = int(temp)**2
+            score.append(temp)
+            temp = ''
+        elif i == 'T':
+            temp = int(temp)**3
+            score.append(temp)
+            temp = ''
+        elif i == '*':
+            if len(score) > 1:
+                score[-2] = score[-2] * 2
+                score[-1] = score[-1] * 2
+            else:
+                score[-1] = score[-1] * 2
+        elif i == '#':
+            score[-1] = score[-1] * -1
+        
+    return sum(score)
+            
+  
+// 다른 사람의 풀이
+
+def solution(dartResult):
+    point = []
+    answer = []
+    dartResult = dartResult.replace('10','k')
+    point = ['10' if i == 'k' else i for i in dartResult]
+    print(point)
+
+    i = -1
+    sdt = ['S', 'D', 'T']
+    for j in point:
+        if j in sdt :
+            answer[i] = answer[i] ** (sdt.index(j)+1)
+        elif j == '*':
+            answer[i] = answer[i] * 2
+            if i != 0 :
+                answer[i - 1] = answer[i - 1] * 2
+        elif j == '#':
+            answer[i] = answer[i] * (-1)
+        else:
+            answer.append(int(j))
+            i += 1
+    return sum(answer)
+    
