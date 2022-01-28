f687e834a2b5b609010d4a66ea39c2d63156796c
Park Chan <60590737+cksl0830@users.noreply.github.com>
2022-01-28 19:16:29 +0900

    1.28일

--- 프로그래머스/Level1/자릿수 더하기.py
+++ 프로그래머스/Level1/자릿수 더하기.py
@@ -8,9 +8,13 @@ def solution(N):
     return answer
 
  
-// 다른 사람의 풀이 (재귀 사용)
+// 다른 사람의 풀이 (재귀 사용) # 아주 좋은 방법.. 생각을 더 많이 해보자!
 
 def sum_digit(number):
     if number < 10:
         return number;
     return (number % 10) + sum_digit(number // 10) 
+
+// 다른 사람의 풀이2 # 간단하게 구현할 수 있음에도 좀 돌아갔던 나의 풀이.. 
+def sum_digit(number):
+    return sum([int(i) for i in str(number)])
