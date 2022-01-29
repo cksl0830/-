d74a2bafb45814db70c7ac7ecbfaf2d7fa12553f
Park Chan <60590737+cksl0830@users.noreply.github.com>
2022-01-29 14:17:03 +0900

    1.29일

---
+++ 프로그래머스/Level1/짝수홀수.py
@@ -0,0 +1,12 @@
+def solution(num):
+    ans1="Even"
+    ans2="Odd"
+    return ans1 if num%2==0 else ans2
+
+
+// 다른 사람의 풀이 # 논리 연산자를 잘 활용한 것 같아서 가져옴  
+def evenOrOdd(num):
+    if (num%2):
+        return "Odd"
+    else:
+        return "Even"
