357e2d33ad3cf887f88fc3e10ff127bbb62552a5
Park Chan <60590737+cksl0830@users.noreply.github.com>
2022-06-07 15:48:42 +0900

    문자열

--- 카카오기출/Level2/오픈채팅방.java
+++ 카카오기출/Level2/오픈채팅방.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] record) {
         HashMap<String,String> map=new HashMap();
-        ArrayList<String> arr = new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>();
         
         for(int i=0; i<record.length; i++){
             String[] temp = record[i].split(" ");
@@ -15,14 +15,17 @@ class Solution {
         for(int i=0; i<record.length; i++){
             String[] temp = record[i].split(" ");
             if(temp[0].equals("Enter")){
-                arr.add(map.get(temp[1]) + "님이 들어왔습니다.");
+                list.add(map.get(temp[1]) + "님이 들어왔습니다.");
             }
             else if(temp[0].equals("Leave")){
-                arr.add(map.get(temp[1]) + "님이 나갔습니다.");
+                list.add(map.get(temp[1]) + "님이 나갔습니다.");
             }
         }
         String[] answer;
-        answer = arr.toArray(new String[arr.size()]);
+        answer = list.toArray(new String[list.size()]);
         return answer;
     }
 }
+
+
+
