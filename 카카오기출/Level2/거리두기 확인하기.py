5ff536a11082e59243ab8d32362857b7f319fbfb
Park Chan <60590737+cksl0830@users.noreply.github.com>
2022-06-08 12:16:50 +0900

    BFS

--- 카카오기출/Level2/거리두기 확인하기.py
+++ 카카오기출/Level2/거리두기 확인하기.py
@@ -1,4 +1,6 @@
 from collections import deque
+dx = [-1, 1, 0, 0]  
+dy = [0, 0, -1, 1]  
 
 def bfs(graph):
     p = []
@@ -8,39 +10,35 @@ def bfs(graph):
             if graph[i][j] == 'P':
                 p.append((i, j))
     
-
+    visited = [[0]*5 for i in range(5)]   # 방문 처리 리스트
+    distance = [[0]*5 for i in range(5)]  # 경로 길이 리스트
+    q=deque()
+    
     for x,y in p:
-        q=deque()
         q.append((x,y))
-        visited = [[0]*5 for i in range(5)]   # 방문 처리 리스트
-        distance = [[0]*5 for i in range(5)]  # 경로 길이 리스트
         visited[x][y] = 1
-        
-        dx = [-1, 1, 0, 0]  
-        dy = [0, 0, -1, 1]  
             
         while q:
             x, y = q.popleft()
-
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
-
                 if 0<=nx<5 and 0<=ny<5 and visited[nx][ny] == 0:
+                    if graph[nx][ny]=='X':
+                        continue
+                    if graph[nx][ny] == 'P' and distance[x][y] <=1:
+                        return 0
                     if graph[nx][ny] == 'O':
                         q.append((nx, ny))
                         visited[nx][ny] = 1
                         distance[nx][ny] = distance[x][y] + 1
-                    
-                    if graph[nx][ny] == 'P' and distance[x][y] <= 1:
-                        return 0
+                        
     return 1
-
 
 def solution(places):
     answer = []
     
-    for i in places:
-        answer.append(bfs(i))
+    for place in places:
+        answer.append(bfs(place))
     
     return answer
