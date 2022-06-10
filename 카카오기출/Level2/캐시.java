9542dc17c1541a80b22d840b556092f2e9315c1c
Park Chan <60590737+cksl0830@users.noreply.github.com>
2022-06-10 17:58:57 +0900

    QUEUE

---
+++ 카카오기출/Level2/캐시.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    
+    public int solution(int cacheSize, String[] cities) {
+        if(cacheSize == 0) return 5 * cities.length;
+        
+        int answer = 0;
+        
+        LinkedList<String> cache = new LinkedList<>();
+        
+        for(int i = 0 ; i < cities.length ; i++){
+            String city = cities[i].toLowerCase();
+            
+            // cache hit
+            if(cache.remove(city)){
+                cache.addLast(city);
+                answer += 1;
+            
+            // cache miss
+            } else {
+                if(cache.size() == cacheSize){
+                    cache.removeFirst();
+                }
+                cache.addLast(city);
+                answer += 5;
+            }
+        }
+        return answer;
+    }
+}
